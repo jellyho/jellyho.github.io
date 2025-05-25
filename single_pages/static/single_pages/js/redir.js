6a93898f6c715889d5e79e1aac29137d144b2a3a
jellyho <jellyho@naver.com>
2025-05-25 20:33:52 +0900

    update

--- single_pages/static/single_pages/js/redir.js
+++
@@ -1,2 +0,0 @@
-document.getElementById('loginurl').setAttribute('href','accounts/login/?redir='+window.location.href);
-document.getElementById('logouturl').setAttribute('href','accounts/logout/?redir='+window.location.href);
